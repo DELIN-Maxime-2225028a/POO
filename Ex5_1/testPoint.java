3f01df43a9499041a2672020ce678cb7b6aba4ec
DELIN-Maxime <98822652+DELIN-Maxime@users.noreply.github.com>
2022-03-18 12:07:47 +0100

    Create testPoint.java

---
+++ Ex5_1/testPoint.java
@@ -0,0 +1,11 @@
+package Ex5_1;
+
+public class testPoint {
+	   public static void main(String[] args) {
+	      point p1 = new point(10, 20);   // Construct a Point
+	      System.out.println(p1);
+	      // Try setting p1 to (100, 10).
+	      point p2 = new point(100, 10);
+	      System.out.println(p2);
+	   }
+	}
