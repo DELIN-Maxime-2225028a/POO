bda98795bf7d75901114477b54baff44a199638e
DELIN-Maxime <98822652+DELIN-Maxime@users.noreply.github.com>
2022-03-18 12:06:47 +0100

    Create testLineSub.java

---
+++ Ex5_1/testLineSub.java
@@ -0,0 +1,15 @@
+package Ex5_1;
+
+public class testLineSub {
+	   public static void main(String[] args) {
+	      line l1 = new line(0, 0, 3, 4);
+	      System.out.println(l1);
+	   
+	      point p1 = new point(30,70);
+	      point p2 = new point(10, 90);
+	      line l2 = new line(p1, p2);
+	      System.out.println(l2);
+	      System.out.println(l2.getLength());
+	      System.out.println(l1.getGradient());
+	   }
+	}
